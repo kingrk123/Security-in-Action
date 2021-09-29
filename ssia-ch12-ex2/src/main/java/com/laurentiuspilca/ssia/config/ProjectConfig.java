c5681e3ccf649efc0cf04046f9ec123e66640fef
kingrk123 <35467049+kingrk123@users.noreply.github.com>
2021-09-29 00:14:57 -0700

    first commit

---
+++ ssia-ch12-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -0,0 +1,35 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2Login(c -> {
+            c.clientRegistrationRepository(clientRepository());
+        });
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    private ClientRegistrationRepository clientRepository() {
+        var c = clientRegistration();
+        return new InMemoryClientRegistrationRepository(c);
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("35cbfc41437daedea0d3")
+                .clientSecret("ad5046225bea2fa9a648c395346dfac9f9973369")
+                .build();
+    }
+}
